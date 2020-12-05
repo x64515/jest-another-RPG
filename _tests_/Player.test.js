953668d05841aea3fe4ecc1f2b8194f47f4e0fb3
Xavier <X64515@yahoo.com>
2020-12-05 17:20:09 -0600

    add inventory and potion require

---
+++ _tests_/Player.test.js
@@ -0,0 +1,18 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+console.log(new Potion());
+
+jest.mock('../lib/Potion');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+  });
